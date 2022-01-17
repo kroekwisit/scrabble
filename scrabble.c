d845f44f9d56e489254482e92f8da745d137307b
kroekwisit <82571425+kroekwisit@users.noreply.github.com>
2022-01-17 19:38:38 +0700

    Update scrabble.c

--- scrabble.c
+++ scrabble.c
@@ -1,18 +1,21 @@
 #include <ctype.h>
-#include <cs50.h>
 #include <stdio.h>
 #include <string.h>
 
 // Points assigned to each letter of the alphabet
 int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 
-int compute_score(string word);
+int compute_score(char* word);
 
 int main(void)
 {
     // Get input words from both players
-    string word1 = get_string("Player 1: ");
-    string word2 = get_string("Player 2: ");
+    char word1[256];
+    printf("Player 1: ");
+    scanf("%s", word1);
+    char word2[256];
+    printf("Player 2: ");
+    scanf("%s", word2);
 
     // Score both words
     int score1 = compute_score(word1);
@@ -33,7 +36,7 @@ int main(void)
     }
 }
 
-int compute_score(string word)
+int compute_score(char* word)
 {
     // TODO: Compute and return score for string
     int p = 0;
